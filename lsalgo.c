50f6056f57a8c463b76b7ac95434cffafe67bbc7
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-03-07 18:27:35 +0530

    Update lsalgo.c
    Fixed some edge cases.

--- lsalgo.c
+++ lsalgo.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
 
-int main(){
-	int a[6]={3,5,7,1,9,4};
-	int i,n;
+int main()
+{
+	int a[8] = {5, -3, 0, 7, -2, 0, 6, -4};
+	int i = 0;
+	int n;
 	int temp;
-	int sortedIndex=0;
-	int k = 7;
+	int counter = 0;
+	int k = 8;
+	int max = a[0];
+	int min = a[0];
+
+
+	for(i=0;i<k;i++){
+		if(min>a[i]){
+			min = a[i];
+		}
+
+		if(max<a[i]){
+			max = a[i];
+		}
+	}
 	
-	for(n=1;n<=k;n++){
-		if(n!=a[n-1]){
-			for(i=sortedIndex;i<k;i++){
-				if(a[i]==n){
-					temp = a[i];
-					a[i]=a[sortedIndex];
-					a[sortedIndex]=temp;
-					sortedIndex++;
-					break;
-					}
-				}
+	for(n=min;n<=max;n++){
+		for(i=0;i<k;i++){
+			if(a[i]==n){
+				temp = a[i];
+				a[i]=a[counter];
+				a[counter]=temp;
+				counter++;
 			}
-		else{
-			sortedIndex++;
-		    }
+		}
 	}
 	printf("The Sorted Array is: \n");
-	for(i=0;i<k-1;i++){
+	for(i=0;i<k;i++){
 		printf("%d  ", a[i]);
 	}
 
