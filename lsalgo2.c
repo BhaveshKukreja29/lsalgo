ceb9a65fe654a4500f9d9f70bb015c8b852ecb45
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-03-24 17:56:39 +0530

    Update lsalgo2.c
    Just added some comments

--- lsalgo2.c
+++ lsalgo2.c
@@ -14,7 +14,8 @@ int main(){
 
     printf("Hello\n");
 
-    for(i=0;i<k1;i++){
+	
+    for(i=0;i<k1;i++){               //This loop selects the minimum and maximum values to start the sorting process below.
 		if(min>a[i]){
 			min = a[i];
 		}
@@ -22,36 +23,36 @@ int main(){
 		if(max<a[i]){
 			max = a[i];
 		}
-	}
+	}   
     printf("min is %d and max is %d\n",min,max);
     
     
     do{
         n=min;
-		for(i=0;i<k1;i++){
+		for(i=0;i<k1;i++){                   //swapping logic starts here
 			if(a[i]==n){
-				b[counter]=a[i];
+				b[counter]=a[i];       //basically if you find the minimum at a[i] copy that in b[counter]
                 for(j=i;j<k1-1;j=j+1){
-                    a[j]=a[j+1];
+                    a[j]=a[j+1];                        //this loop deletes the copied element from a[i] by shifting it's succeeding indexes one step behind
                 }
-                k1--;
-                counter++;
+                k1--;                                //to reduce to size of a[i] since we deleted an element
+                counter++;                           //points to the location for next element to be stored.
 			}
-		}
+		}                                   //swapping logic ends
         min = a[0];
         for(j=0;j<k1;j++){
-		            if(min>a[j]){
+		            if(min>a[j]){                  //to select the new minimum value for next iteration
 			            min = a[j];
 		            }
         	    }
-        if(k1==1){
+        if(k1==1){                        //k1==1 means only one element remains, added this because the loop didn't consider the last remaining element.
             b[counter]=a[0];
         }
 
     }while(min!=max);
 
-    printf("The Sorted Array is: \n");
-	for(i=0;i<k2;i++){
+    printf("The Sorted Array is: \n");      
+	for(i=0;i<k2;i++){                 //prints the array b
 		printf("%d ", b[i]);
 	}
 
